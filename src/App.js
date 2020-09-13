ecc07bdb083a2ce1d6f5107c0b6ad6109070f680
Nathan Andrade <nathandulino2003@gmail.com>
2020-09-12 23:46:06 -0300

    ToDo clone

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Take dogs for walk', 'Take dogs for running', 'hi programmers']);
+  const [input, setInput] = useState('');
+
+  // constante usada para adicionar um novo TODO
+  const addTodo = (event => {
+    //this will fire off when we click the button
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
+  })
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <input />
-      <button>Add Todo</button>
+      <h1>Creating a TODO App!</h1>
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)} />
+        <button type="submit" onClick={addTodo}>Add Todo</button>
+      </form>
 
       <ul>
-        <li>Take dogs for walk</li>
-        <li>Take dogs for running</li>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
       </ul>
     </div>
   );
